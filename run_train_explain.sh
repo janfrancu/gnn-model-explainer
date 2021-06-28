23125c6ce09aa6ab0bb13f074cca3093c768faed
Jan Francu <jan.francu@gmail.com>
2021-06-28 08:05:37 +0200

    Expose expl method in bash script.

--- run_train_explain.sh
+++ run_train_explain.sh
@@ -13,5 +13,10 @@ module load Python/3.8.2-GCCcore-9.3.0
 # load virtualenv
 source ./pyenv_gnn/bin/activate
 
-python ./train.py --dataset=$DATASET
-python ./explainer_main.py --dataset=$DATASET --model=$EXP_MODEL
+if [ "$EXP_MODEL" == "attn" ]; then
+    python ./train.py --dataset=$DATASET --method=$EXP_MODEL
+    python ./explainer_main.py --dataset=$DATASET --model=$EXP_MODEL --method=$EXP_MODEL
+else
+    python ./train.py --dataset=$DATASET
+    python ./explainer_main.py --dataset=$DATASET --model=$EXP_MODEL
+fi
