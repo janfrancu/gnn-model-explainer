3ee53b5b98863a71cefea2bb9b40278859e9e354
Jan Francu <jan.francu@gmail.com>
2021-07-20 22:13:25 +0200

    Exporting explainer results/weights.

--- export_weights.py
+++ export_weights.py
@@ -29,7 +29,13 @@ import models
 
 from train import train_node_classifier
 
-def export_weights_and_prediction_npy(model, filename, G, labels):
+### explainer specific
+import explainer_main
+from explainer import explain
+
+###
+
+def export_weights_and_prediction_npy(filename, model, G, labels, train_idx=None):
     W1 = model.conv_first._parameters['weight'].detach().numpy()
     b1 = model.conv_first._parameters['bias'].detach().numpy()
     W2 = model.conv_block[0]._parameters['weight'].detach().numpy()
@@ -69,12 +75,116 @@ def export_weights_and_prediction_npy(model, filename, G, labels):
     adj_att = adj_att.detach().numpy()
     labels = labels.detach().numpy()
 
-    np.savez(filename, W1=W1, W2=W2, W3=W3, Wp=Wp, 
-                       b1=b1, b2=b2, b3=b3, bp=bp,
-                       gW1=gW1, gW2=gW2, gW3=gW3, gWp=gWp, 
-                       gb1=gb1, gb2=gb2, gb3=gb3, gbp=gbp,  
-                       adj=adj, x=x, ypred=ypred, 
-                       labels=labels, loss=loss)
+    if train_idx:
+        np.savez(filename, W1=W1, W2=W2, W3=W3, Wp=Wp, 
+                           b1=b1, b2=b2, b3=b3, bp=bp,
+                           gW1=gW1, gW2=gW2, gW3=gW3, gWp=gWp, 
+                           gb1=gb1, gb2=gb2, gb3=gb3, gbp=gbp,  
+                           adj=adj, x=x, ypred=ypred, 
+                           labels=labels, loss=loss, train_idx=np.array(train_idx))
+    else:
+        np.savez(filename, W1=W1, W2=W2, W3=W3, Wp=Wp, 
+                           b1=b1, b2=b2, b3=b3, bp=bp,
+                           gW1=gW1, gW2=gW2, gW3=gW3, gWp=gWp, 
+                           gb1=gb1, gb2=gb2, gb3=gb3, gbp=gbp,  
+                           adj=adj, x=x, ypred=ypred, 
+                           labels=labels, loss=loss)
+
+
+def export_explainer_weights(filename, seed, node_idx, cg_dict, model):
+    exp_args = explainer_main.arg_parse()
+    writer = None
+    graph_mode = False
+    graph_idx = 0
+    unconstrained = False
+
+    explainer = explain.Explainer(
+        model=model,
+        adj=cg_dict["adj"],
+        feat=cg_dict["feat"],
+        label=cg_dict["label"],
+        pred=cg_dict["pred"],
+        train_idx=cg_dict["train_idx"],
+        args=exp_args,
+        writer=writer,
+        print_training=False,
+        graph_mode=graph_mode,
+        graph_idx=graph_idx,
+    )
+
+    # mimicking Explainer.explain method to construct ExplainModule
+    node_idx_new, sub_adj, sub_feat, sub_label, neighbors = explainer.extract_neighborhood(node_idx, graph_idx)
+    sub_label = np.expand_dims(sub_label, axis=0)
+    sub_adj = np.expand_dims(sub_adj, axis=0)
+    sub_feat = np.expand_dims(sub_feat, axis=0)
+
+    adj   = torch.tensor(sub_adj, dtype=torch.float)
+    x     = torch.tensor(sub_feat, requires_grad=True, dtype=torch.float)
+    label = torch.tensor(sub_label, dtype=torch.long)
+
+    pred_label = np.argmax(explainer.pred[graph_idx][neighbors], axis=1)
+
+    torch.random.manual_seed(seed) # fix seed for reproducibility
+    explain_module = explain.ExplainModule(
+            adj=adj,
+            x=x,
+            model=model,
+            label=label,
+            args=exp_args,
+            writer=writer,
+            graph_idx=graph_idx,
+            graph_mode=graph_mode,
+        )
+
+    print("node label: ", explainer.label[graph_idx][node_idx])
+    print("neigh graph idx: ", node_idx, node_idx_new)
+    print("Node predicted label: ", pred_label[node_idx_new])
+
+    ### grad method mask
+    explain_module.zero_grad()
+    adj_grad, feat_grad = explain_module.adj_feat_grad(node_idx_new, pred_label[node_idx_new])
+    adj_grad = torch.abs(adj_grad)[graph_idx]
+    masked_adj = adj_grad + adj_grad.t()
+    masked_adj = nn.functional.sigmoid(masked_adj)
+    masked_adj = masked_adj.cpu().detach().numpy() * sub_adj.squeeze()
+    feat_mask = feat_grad.detach().numpy()
+
+    ypred, _ = model(x, adj) # prediction on the subgraph, used for further checks
+    ypred = ypred.detach().numpy()
+
+    np.savez(filename + "_grad", node_idx=node_idx, node_idx_new=node_idx_new, neighbors=neighbors,
+        pred_label=pred_label, ypred=ypred, sub_label=sub_label, sub_feat=sub_feat, sub_adj=sub_adj,
+        masked_adj=masked_adj, feat_mask=feat_mask)
+
+    ### explainer's loss and gradient (first iteration output only)
+    # what do we need for the loss computation
+    # ypred, pred_label, label, edge_mask, feat_mask, features and original adjacency
+    explain_module.zero_grad()
+    explain_module.optimizer.zero_grad()
+    ypred, _ = explain_module(node_idx_new, unconstrained=unconstrained)
+    
+
+    loss = explain_module.loss(ypred, pred_label, node_idx_new, 0)
+    loss.backward()
+
+    edge_mask = explain_module._parameters['mask'].detach().numpy()
+    feat_mask = explain_module._parameters['feat_mask'].detach().numpy()
+    loss = loss.detach().numpy()
+
+    grad_edge_mask = explain_module._parameters['mask'].grad.numpy()
+    grad_feat_mask = explain_module._parameters['feat_mask'].grad.numpy()
+
+    ypred = ypred.detach().numpy()
+   
+    masked_adj = explain_module.masked_adj.detach().numpy()
+
+
+    np.savez(filename + "_exp", node_idx=node_idx, node_idx_new=node_idx_new, neighbors=neighbors,
+        loss=loss, ypred=ypred, pred_label=pred_label,
+        sub_label=sub_label, sub_feat=sub_feat, sub_adj=sub_adj,
+        masked_adj=masked_adj, edge_mask=edge_mask, feat_mask=feat_mask, 
+        grad_edge_mask=grad_edge_mask, grad_feat_mask=grad_feat_mask)
+
 
 
 def syn_task1(args, writer=None):
@@ -94,10 +204,13 @@ def syn_task1(args, writer=None):
         args=args,
     )
 
-    export_weights_and_prediction_npy(model, "./log/model_export/syn1:{}_init".format(args.seed), G, labels)
+    export_weights_and_prediction_npy("./log/model_export/syn1:{}_init".format(args.seed), model, G, labels)
 
-    train_node_classifier(G, labels, model, args, writer=writer)
-    export_weights_and_prediction_npy(model, "./log/model_export/syn1:{}_trained".format(args.seed), G, labels)
+    cg_dict = train_node_classifier(G, labels, model, args, writer=writer)
+    export_weights_and_prediction_npy("./log/model_export/syn1:{}_trained".format(args.seed), model, G, labels, cg_dict["train_idx"])
+
+    node_idx = 300
+    export_explainer_weights("./log/model_export/syn1:{}_explain".format(args.seed), args.seed, node_idx, cg_dict, model)
 
 
 def syn_task2(args, writer=None):
@@ -116,10 +229,13 @@ def syn_task2(args, writer=None):
         args=args,
     )
 
-    export_weights_and_prediction_npy(model, "./log/model_export/syn2:{}_init".format(args.seed), G, labels)
+    export_weights_and_prediction_npy("./log/model_export/syn2:{}_init".format(args.seed), model, G, labels)
 
-    train_node_classifier(G, labels, model, args, writer=writer)
-    export_weights_and_prediction_npy(model, "./log/model_export/syn2:{}_trained".format(args.seed), G, labels)
+    cg_dict = train_node_classifier(G, labels, model, args, writer=writer)
+    export_weights_and_prediction_npy("./log/model_export/syn2:{}_trained".format(args.seed), model, G, labels, cg_dict["train_idx"])
+
+    node_idx = 300
+    export_explainer_weights("./log/model_export/syn2:{}_explain".format(args.seed), args.seed, node_idx, cg_dict, model)
 
 
 def syn_task3(args, writer=None):
@@ -140,10 +256,13 @@ def syn_task3(args, writer=None):
         args=args,
     )
 
-    export_weights_and_prediction_npy(model, "./log/model_export/syn3:{}_init".format(args.seed), G, labels)
+    export_weights_and_prediction_npy("./log/model_export/syn3:{}_init".format(args.seed), model, G, labels)
 
-    train_node_classifier(G, labels, model, args, writer=writer)
-    export_weights_and_prediction_npy(model, "./log/model_export/syn3:{}_trained".format(args.seed), G, labels)
+    cg_dict = train_node_classifier(G, labels, model, args, writer=writer)
+    export_weights_and_prediction_npy("./log/model_export/syn3:{}_trained".format(args.seed), model, G, labels, cg_dict["train_idx"])
+
+    node_idx = 301
+    export_explainer_weights("./log/model_export/syn3:{}_explain".format(args.seed), args.seed, node_idx, cg_dict, model)
 
 
 def syn_task4(args, writer=None):
@@ -164,10 +283,14 @@ def syn_task4(args, writer=None):
         args=args,
     )
 
-    export_weights_and_prediction_npy(model, "./log/model_export/syn4:{}_init".format(args.seed), G, labels)
+    export_weights_and_prediction_npy("./log/model_export/syn4:{}_init".format(args.seed), model, G, labels)
 
-    train_node_classifier(G, labels, model, args, writer=writer)
-    export_weights_and_prediction_npy(model, "./log/model_export/syn4:{}_trained".format(args.seed), G, labels)
+    cg_dict = train_node_classifier(G, labels, model, args, writer=writer)
+    export_weights_and_prediction_npy("./log/model_export/syn4:{}_trained".format(args.seed), model, G, labels, cg_dict["train_idx"])
+
+    node_idx = 511
+    export_explainer_weights("./log/model_export/syn4:{}_explain".format(args.seed), args.seed, node_idx, cg_dict, model)
+
 
 
 def syn_task5(args, writer=None):
@@ -189,17 +312,19 @@ def syn_task5(args, writer=None):
         args=args,
     )
 
-    export_weights_and_prediction_npy(model, "./log/model_export/syn5:{}_init".format(args.seed), G, labels)
+    export_weights_and_prediction_npy("./log/model_export/syn5:{}_init".format(args.seed), model, G, labels)
 
-    train_node_classifier(G, labels, model, args, writer=writer)
-    export_weights_and_prediction_npy(model, "./log/model_export/syn5:{}_trained".format(args.seed), G, labels)
+    cg_dict = train_node_classifier(G, labels, model, args, writer=writer)
+    export_weights_and_prediction_npy("./log/model_export/syn5:{}_trained".format(args.seed), model, G, labels, cg_dict["train_idx"])
 
+    node_idx = 512
+    export_explainer_weights("./log/model_export/syn5:{}_explain".format(args.seed), args.seed, node_idx, cg_dict, model)
 
 
 # the data should be the same - export the adjacency matrix and others
 args = prog_args = configs.arg_parse()
 
-for seed in range(1):
+for seed in range(5):
     prog_args.seed = seed
     for i in range(5):
        fun = 'syn_task' + str(i+1)
